3303a9b3294b09af7fe7f98a8f22dfda17c0d6f0
Muhammadjon Abduvahobov <megacoderuzb@gmail.com>
2022-12-16 17:03:04 +0500

    ozbeca

--- app.js
+++ app.js
@@ -6,8 +6,10 @@ let onion = document.querySelector(".btn-o");
 let salad = document.querySelector(".btn-s");
 let tomato = document.querySelector(".btn-t");
 let pickle = document.querySelector(".btn-p");
+localStorage.setItem("non", 2)
 cheese.addEventListener("click", (e) => {
   e.preventDefault();
+  localStorage.setItem("cheese", 2)
   const sir = document.createElement("div");
   sir.innerHTML = `<img src="./burger-layers/cheese.svg" alt="cheese" />`;
   sir.style.height = "30px";
@@ -20,10 +22,12 @@ cheese.addEventListener("click", (e) => {
     masalliq.removeChild(sir);
     hisob -= 2;
     total.textContent = `Total:  ${hisob}$ dollar`;
+    localStorage.removeItem("cheese")
   });
 });
 meat.addEventListener("click", (e) => {
   e.preventDefault();
+  localStorage.setItem("meat", 5)
   hisob += 5;
   total.textContent = `Total:  ${hisob}$ dollar`;
   const meat = document.createElement("div");
@@ -36,10 +40,13 @@ meat.addEventListener("click", (e) => {
     masalliq.removeChild(meat);
     hisob -= 5;
     total.textContent = `Total:  ${hisob}$ dollar`;
+    localStorage.removeItem("meat")
   });
+
 });
 onion.addEventListener("click", (e) => {
   e.preventDefault();
+  localStorage.setItem("onion",1)
   const onion = document.createElement("div");
   hisob += 1;
   total.textContent = `Total:  ${hisob}$ dollar`;
@@ -52,10 +59,12 @@ onion.addEventListener("click", (e) => {
     masalliq.removeChild(onion);
     hisob -= 1;
     total.textContent = `Total:  ${hisob}$ dollar`;
+    localStorage.removeItem("onion")
   });
 });
 salad.addEventListener("click", (e) => {
   e.preventDefault();
+  localStorage.setItem("salad", 1)
   const salad = document.createElement("div");
   hisob += 1;
   total.textContent = `Total:  ${hisob}$ dollar`;
@@ -68,10 +77,12 @@ salad.addEventListener("click", (e) => {
     masalliq.removeChild(salad);
     hisob -= 1;
     total.textContent = `Total:  ${hisob}$ dollar`;
+    localStorage.removeItem("salad")
   });
 });
 tomato.addEventListener("click", (e) => {
   e.preventDefault();
+  localStorage.setItem("tomato", 2)
   const tomato = document.createElement("div");
   hisob += 2;
   total.textContent = `Total:  ${hisob}$ dollar`;
@@ -84,10 +95,12 @@ tomato.addEventListener("click", (e) => {
     masalliq.removeChild(tomato);
     hisob -= 2;
     total.textContent = `Total:  ${hisob}$ dollar`;
+    localStorage.removeItem("tomato")
   });
 });
 pickle.addEventListener("click", (e) => {
   e.preventDefault();
+  localStorage.setItem("pickle", 3)
   const pickle = document.createElement("div");
   hisob += 3;
   total.textContent = `Total:  ${hisob}$ dollar`;
@@ -100,6 +113,7 @@ pickle.addEventListener("click", (e) => {
     masalliq.removeChild(pickle);
     hisob -= 3;
     total.textContent = `Total:  ${hisob}$ dollar`;
+    localStorage.removeItem("pickle")
   });
 });
 let hisob = 2;
@@ -111,6 +125,8 @@ retry.addEventListener("click", (e) => {
   masalliq.innerHTML = "";
   hisob = 2;
   total.textContent = `Total:  ${hisob}$ dollar`;
+  localStorage.clear()
+  localStorage.setItem("non", 2)
 });
 let send = document.querySelector(".send");
 let queue = document.querySelector(".queue");
@@ -118,4 +134,6 @@ send.addEventListener("click", (e) => {
   e.preventDefault();
   let random = Math.floor(Math.random() * 25) + 10;
   queue.innerHTML = `Your queue is ${random}`;
+  localStorage.setItem("itogo", hisob)
+  localStorage.setItem("queue", random)
 });
